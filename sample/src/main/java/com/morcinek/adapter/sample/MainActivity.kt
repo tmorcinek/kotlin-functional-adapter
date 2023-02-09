7eff88e5f46b054b68cacefba00581ea66acbb0f
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2023-02-09 18:39:52 +0100

    Adding grid and names.

--- sample/src/main/java/com/morcinek/adapter/sample/MainActivity.kt
+++ sample/src/main/java/com/morcinek/adapter/sample/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayoutMediator
-import com.morcinek.android.HasKey
-import com.morcinek.android.customAdapter
-import com.morcinek.android.itemCallback
-import com.morcinek.android.list
+import com.morcinek.android.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.vh_city.view.*
+import kotlinx.android.synthetic.main.vh_city.view.name
+import kotlinx.android.synthetic.main.vh_name.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,12 +28,36 @@ class MainActivity : AppCompatActivity() {
                         submitList(cities)
                     }
                 }
+                itemView<RecyclerView>(R.layout.recycler_view) {
+                    setup {
+                        grid(2) { setupSpanSizeLookup { position -> if (position % 3 == 0) 2 else 1 } }
+                        adapter(listAdapter<City>(itemCallback()) {
+                            resId(R.layout.vh_city)
+                            onBind { position, item ->
+                                number.text = "${position + 1}."
+                                name.text = item.name
+                            }
+                            submitList(cities)
+                        })
+                    }
+                }
+                itemView<RecyclerView>(R.layout.recycler_view) {
+                    list<String>(itemCallback { areItemsTheSame { s, s2 -> s == s2 } }){
+                        resId(R.layout.vh_name)
+                        onBind { _, item ->
+                            name.text = item
+                        }
+                        submitList(names)
+                    }
+                }
             }
 
         }
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = when (position) {
-                0 -> "Cities"
+                0 -> "Cities List"
+                1 -> "Cities Grid"
+                2 -> "Names"
                 else -> "Other"
             }
         }.attach()
@@ -51,7 +74,15 @@ class MainActivity : AppCompatActivity() {
         City("Hamburg"),
         City("London"),
         City("Liverpool"),
+        City("Manchester"),
+        City("Paris"),
+        City("Milan"),
+        City("Turin"),
+        City("Moscow"),
+        City("Moscow"),
     )
+
+    private val names = listOf("Tomek", "Basia", "Kamil", "Krzysiu", "Karolina", "Beata", "Marek", "Grzegorz", "Mikolaj", "Wiktor")
 }
 
 
